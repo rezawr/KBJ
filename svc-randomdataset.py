155bae50f46796d1ff0e7ba4fa955e0645108aec
Reza Wahyu Ramadhan <reza.wahyu@tech.indosterling.com>
2023-06-02 22:13:00 +0700

    s

--- svc-randomdataset.py
+++ svc-randomdataset.py
@@ -16,33 +16,32 @@ if __name__ == "__main__":
     result = []
 
     # Looping to find how number of datasets impacted on accuracy
-    for x in ([1000, 10000, 20000]):
-        # Load dataset
-        df = pd.read_csv('datasets/movie.csv')
+    # Load dataset
+    df = pd.read_csv('datasets/movie.csv')
 
-        # Splitting Dataset
-        X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], random_state=42, train_size=x)
+    # Splitting Dataset
+    X_train, X_test, y_train, y_test = train_test_split(df['text'], df['label'], random_state=42, train_size=1000)
 
-        # Build the model
-        textclassifier = Pipeline([
-            ('vect', CountVectorizer()),
-            ('tfidf', TfidfTransformer()),
-            ('svc', SVC(kernel='linear', C=1, random_state=0, verbose=1))
-        ])
+    # Build the model
+    textclassifier = Pipeline([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('svc', SVC(kernel='linear', C=1, random_state=0, verbose=1))
+    ])
 
-        # Train the data
-        textclassifier.fit(X_train, y_train)
+    # Train the data
+    textclassifier.fit(X_train, y_train)
 
-        # Scoring parameter that we want to analysis
-        scoring = ['precision_macro', 'recall_macro', 'f1_macro', 'accuracy']
-        # 10 fold cross validation
-        scores = cross_validate(textclassifier, df['text'], df['label'], scoring=scoring)
+    # Scoring parameter that we want to analysis
+    scoring = ['precision_macro', 'recall_macro', 'f1_macro', 'accuracy']
+    # 10 fold cross validation
+    scores = cross_validate(textclassifier, df['text'], df['label'], scoring=scoring)
 
-        # save the result of prediction into variabel result
-        result.append({
-            'dataset': x,
-            'scores': scores
-        })
+    # save the result of prediction into variabel result
+    result.append({
+        'dataset': x,
+        'scores': scores
+    })
 
     # Just tracing
     import pdb;pdb.set_trace()
